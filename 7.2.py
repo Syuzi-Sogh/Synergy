43e3f566ddf459e0340c6468dd75c2b79c4ae088
Syuzi <sogomonansuzi@gmail.com>
2025-04-09 23:47:05 +0500

    11th

--- 7.2.py
+++ 7.2.py
@@ -2,5 +2,5 @@ tmp = 'a  b  c  d e'
 
 for i in range(0, len(tmp)):
     if tmp[i] == ' ' and tmp[i + 1] == ' ':
-        print(tmp.split(' '), end=' ')
+        print(tmp.replace('  ', ' '))
         break
