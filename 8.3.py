bd2719494d42085a2ec4d544632006307e81c6e1
Syuzi <sogomonansuzi@gmail.com>
2025-04-10 21:09:18 +0500

    14th

--- 8.3.py
+++ 8.3.py
@@ -4,13 +4,9 @@ fishmen_weights = list(map(int, input().split()))
 lodka = []
 count_fishmen = 0
 
-# 10 - max_weight
-# 5 - fishmen
-# 4 6 4 2 3
 
 for i in range(len(fishmen_weights)):
-
-    if fishmen_weights[i] < max_weight:
+    if fishmen_weights[i] <= max_weight:
         lodka.append(fishmen_weights[i])
 
         if len(lodka) == 2:
@@ -20,6 +16,8 @@ for i in range(len(fishmen_weights)):
             if lodka[0] + lodka[1] > max_weight and lodka[1] > lodka[0]:
                 count_fishmen += 1
                 del lodka[1]
+        if (len(lodka) == 2):
+            count_fishmen += 1
             lodka.clear()
 
 if len(lodka) > 0:
