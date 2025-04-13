c6b1b05c905e6ef06c684f0e65347ab7b019486b
Syuzi <sogomonansuzi@gmail.com>
2025-04-13 14:29:23 +0500

    17th

---
+++ 13.1.py
@@ -0,0 +1,30 @@
+import random
+
+def pl(x):
+    for i in x:
+        print(i)
+
+n = int(input())
+
+matrix1 = [[0 for x in range(n)] for y in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        matrix1[i][j] = random.randint(0, 100)
+
+print(matrix1)
+matrix2 = [[0 for x in range(n)] for y in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        matrix2[i][j] = random.randint(0, 100)
+
+print(matrix2)
+
+matrix3 = [[0 for x in range(n)] for y in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        matrix3[i][j] = matrix1[i][j] + matrix2[i][j]
+
+pl(matrix3)
