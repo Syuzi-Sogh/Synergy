1512af603fe7ac69774391a7611086c4b8a567c1
Syuzi <sogomonansuzi@gmail.com>
2025-04-19 01:23:03 +0500

    165h

--- 10.1.py
+++ 10.1.py
@@ -12,5 +12,11 @@ pets = {
 }
 print("Это", pets[pet_name]["Вид питомца"], end=' ')
 print("по кличке", f'"{pet_name}"',end='.')
-print("Возраст питомца:", pets[pet_name]["Возраст питомца"], end='.')
+if age % 10 == 2 or age % 10 == 3 or age % 10 == 4:
+    print("Возраст питомца:", pets[pet_name]["Возраст питомца"], "года", end='.')
+elif age % 10 == 1:
+    print("Возраст питомца:", pets[pet_name]["Возраст питомца"], "год", end='.')
+else:
+    print("Возраст питомца:", pets[pet_name]["Возраст питомца"], "лет", end='.')
+
 print("Имя владельца:", pets[pet_name]["Имя владельца"], end=' ')
